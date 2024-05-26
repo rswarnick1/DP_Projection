633633e60624013ca53611f0d4bdaa438b970ebb
Ryan <rswarnick1@gmail.com>
2024-05-26 12:59:01 -0500

    Committing new class file

---
+++ Classes.py
@@ -0,0 +1,10 @@
+import numpy as np
+import scipy.stats as sts 
+import tensorflow as tf 
+import tensorflow-probability as tfp 
+
+class DP_Projection_Full_Dimension():
+    def __init__(self, data)
+        self.data = data
+        self.
+
